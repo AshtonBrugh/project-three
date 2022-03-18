d09f5d3e05cbf88a3981ac5d7ebba6a52093e5aa
Ashton B <ashtonbrugh@gmail.com>
2022-03-18 11:56:57 -0400

    checking signup

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -20,16 +20,19 @@ class AuthService {
         return !!token && this.isTokenExpired(token);
     }
 
-    isTokenExpired = token => {
+    isTokenExpired(token) {
         try {
           const decoded = decode(token);
-          if (decoded.exp < Date.now() / 1000) {
+          if (decoded.exp < Date.now() / 1000) { // Checking if token is expired. N
             return true;
-          } else return false;
-        } catch (err) {
+          }
+          else
+            return false;
+        }
+        catch (err) {
           return false;
         }
-      };
+      }
 
     
     getToken() {
