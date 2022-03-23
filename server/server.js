a6c6dab36c5fff84b3a7b1c5886ead03b5a8c7ff
Scott Howell <howell.cscott@gmail.com>
2022-03-23 19:38:33 -0400

    accidentally did a +

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
-    +app.get('/*', function (req, res) {
+    app.get('/*', function (req, res) {
         res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
 }
