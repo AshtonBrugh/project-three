790fcad698fee4d853a926f4a9018dd3dbb25a99
Scott Howell <howell.cscott@gmail.com>
2022-03-24 12:58:20 -0400

    found critical error in server.js (32:50-51 & 35:49-50) Also fixed previous change, as it breaks on heroku

--- server/server.js
+++ server/server.js
@@ -29,10 +29,10 @@ startServer();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 // Step 2:
 app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 db.once('open', () => {
     app.listen(PORT, () => {
